2f4e997bfcf21094d003ad5fb9d4c81e1edef788
Kartik-M24 <161120979+Kartik-M24@users.noreply.github.com>
2024-04-18 14:09:28 +1200

    Update Main.java
    Makes sure inputs are in the valid range

--- Main.java
+++ Main.java
@@ -17,31 +17,36 @@ public class Main {
         int largerVal;
         int smallerVal;
 
-        // Determine if a is larger than b
-        if (a >= b) {
-            largerVal = a;
-            smallerVal = b;
-         }
-        else {
-            largerVal = b;
-            smallerVal = a;
+        // Checks for valid inputs
+        if (a > 45 || b > 45 || a < 0 || b < 0) {
+            System.out.println("Please make sure inputs are between 1 and 45 (inclusive)");
         }
+        else {
+            // Determine if a is larger than b
+            if (a >= b) {
+                largerVal = a;
+                smallerVal = b;
+            }
+            else {
+                largerVal = b;
+                smallerVal = a;
+            }
 
-        // Using the sumOfnFibonnaci class
-        sumOfnFibonnaci sum = new sumOfnFibonnaci(largerVal, smallerVal);
-        int[] sums = sum.findSum();
-        int sumLarger = sums[0];
-        int sumSmaller = sums[1];
+            // Using the sumOfnFibonnaci class
+            sumOfnFibonnaci sum = new sumOfnFibonnaci(largerVal, smallerVal);
+            int[] sums = sum.findSum();
+            int sumLarger = sums[0];
+            int sumSmaller = sums[1];
 
-        // Printing out the results
-        System.out.println("Inputs are A: " + a + " B: " + b);
-        System.out.println("Sum of first " + largerVal + " fibonnaci numbers: " + sumLarger);
-        System.out.println("Sum of first " + smallerVal + " fibonnaci numbers: " + sumSmaller);
-        System.out.println("Iteration n:  a = b x q + r");
+            // Printing out the results
+            System.out.println("Inputs are A: " + a + " B: " + b);
+            System.out.println("Sum of first " + largerVal + " fibonnaci numbers: " + sumLarger);
+            System.out.println("Sum of first " + smallerVal + " fibonnaci numbers: " + sumSmaller);
+            System.out.println("Iteration n:  a = b x q + r");
 
-        // Finding the GCD of the two sums
-        int output = gcd(sumLarger, sumSmaller, 1);
-        System.out.println("GCD of the two sums is: " + output);
-
+            // Finding the GCD of the two sums
+            int output = gcd(sumLarger, sumSmaller, 1);
+            System.out.println("GCD of the two sums is: " + output);
+        }
     }
 }
